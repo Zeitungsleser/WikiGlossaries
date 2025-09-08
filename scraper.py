2f5fe9f4d9669446be0c263c1391a754386a4e1d
Zeitungsleser <ju.lg@gmx.de>
2025-09-08 20:59:48 +0200

    Working on it

--- scraper.py
+++ scraper.py
@@ -8,14 +8,12 @@ with open("alt.html") as f:
     for speaker in soup.find_all('a'):
         if speaker.get('aria-label'):
             speaker.extract()
-    words = []
-    descriptions = []
     glossary = {}
-    for dt in soup.find_all('dt'):
-        words.append(dt.get_text().replace(" )", ")"))
-    for dd in soup.find_all('dd'):
-        descriptions.append(dd.get_text())
-    for i, word in enumerate(words):
-        glossary[word] = descriptions[i]
-    with open("alt.json", 'w') as g:
-        json.dump(glossary,g,indent=4)
+    for dl in soup.find_all('dl'):
+        word = ""
+        definition = ""
+        for child in dl.children:
+            if child.name == "dt":
+                glossary[word]                
+    #with open("alt.json", 'w') as g:
+    #    json.dump(glossary,g,indent=4)
